bf71f4aec1dd8fa0d46db735a1bfa84c78db8c73
Kishore-coding <kishorecherrya2z@gmail.com>
2021-01-18 13:20:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+
+    
+</div>
+    
+);
+}
+
+export default App;
+
+        
+   /*   <section className="s1">
+        <div className="main-container">
+          <div className="my-name">
+            <h1>Hi, I'm Kishore</h1>
+          </div>
+
+        <div className="intro-wrapper">
+            <div className="nav-wrapper">
+                <div className="nav-content">
+                    <div className="portfolio">
+                     I am a React JS developer - Fresher
+                    </div>                        
+                                              
+                </div>
+            </div>
+            <div class="left-column">
+                <img  id="profile-pic" src="./images/mypic.jpg" alt="cherry" />
+                                    
+            </div>
+            <div className="right-column">
+                <div className="preview-shadow">
+                    <div id="preview">
+                   
+                        <h3>What I am doing</h3>
+                        <p>I am currently doing some frontend projects to expose and to improve my skills 
+                            in <strong>React JS</strong> Frontend development
+                        </p>
+                        
+                    </div>
+
+                </div>
+               
+            </div>
+        </div>
+                    
+    </div>
+</section>
+
+<section className="s1">
+    <div className="main-container">
+        <h3 style="text-align:center">Some of my past projects</h3>
+
+        <div className="post-wrapper">
+            <div>     
+                <div className="post">
+                    <img className="thumbnail" src="images/car.jpeg" alt="post1"/>
+                    <div className="post-preview">
+                        <h6 className="post-title">Car showroom site</h6>
+                        <p className="post-intro">
+                            This is a simple car showroom site 
+                            where we can view some of the top branded cars over here.
+                        </p>
+                        
+                        <a href="https://cherry-car-showroom.herokuapp.com/" 
+                        target="_blank noopener noreferer">Car Showroom site</a>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className="post">
+                    <img className="thumbnail" src="images/search.jpg" alt="post2"/>
+                    <div className="post-preview">
+                        <h6 className="post-title">Image searching app</h6>
+                        <p className="post-intro">
+                            In this project I used Pixaby API, where we can 
+                            search for the images that we want.
+                        </p>
+                        <a href="https://cherry-image-searcher.herokuapp.com/"
+                          target="_blank noopener noreferer" >Image searching App</a>
+                    </div>
+                </div>  
+            </div>
+
+            <div>
+                <div className="post">
+                    <img className="thumbnail" src="images/todo.jpg" alt="post3" />
+                    <div className="post-preview">
+                        <h6 className="post-title">React-redux-TODO</h6>
+                        <p className="post-intro">
+                        Created a simple TODO list using React JS and Redux, where we 
+                        can add and delete an Item
+                        </p>
+                        <a href="https://cherry-react-redux-todo.herokuapp.com/" target="_blank noopener noreferer">
+                          Todo</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div className="missed-projects">
+        <p>Due to some technical issues I didn't host my <strong> Shopping cart, 
+            Authentication form  and <br> Movie Information App </strong> Project,
+           so you can visit my Github profile to view these projects 
+        </p>
+        <p>Github profile :  <a href="https://github.com/Kishore-coding?tab=repositories" target="_blank noopener noreferer">
+            Github</a> </p>
+    </div>
+
+</section>
+
+
+
+<section className="s2">
+    <div className="main-container">
+        <h4 className="footer">Contact me </h4>
+    
+        <div className="icons">
+            <ul>
+                <li><i className="far fa-envelope"></i> kishorecattie02@gmail.com</li>
+                <li><i className="fab fa-linkedin"></i>
+                    <a href="https://www.linkedin.com/in/kishore-k-r-366b871aa/" target="_blank noreferer noopener"> LinkedIn</a></li>
+                <li><i className="fas fa-phone-square-alt"></i> 9751427841</li>
+                </ul>
+            </div>
+    </div>
+  </section>*/
